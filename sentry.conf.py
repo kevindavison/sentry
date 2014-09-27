1ded10bfe7bffbfd0e24af811d57905aeafde7fb
Ozan Onay <ozan.onay@gmail.com>
2014-09-26 18:16:01 -0700

    Add sentry-github

--- sentry.conf.py
+++ sentry.conf.py
@@ -20,15 +20,16 @@ SENTRY_ALLOW_REGISTRATION = False
 
 SERVER_EMAIL = os.environ.get('SENTRY_EMAIL_FROM', 'root@localhost')
 
-# You should configure the absolute URI to Sentry. It will attempt to guess it if you don't
-# but proxies may interfere with this.
-SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX', '')
+# The absolute URI to Sentry
+SENTRY_URL_PREFIX = os.environ['SENTRY_URL_PREFIX']
 
 SENTRY_WEB_HOST = '0.0.0.0'
 SENTRY_WEB_PORT = 3000
 SENTRY_WEB_OPTIONS = {
-    'workers': 3,  # the number of gunicorn workers
-    'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},  # detect HTTPS mode from X-Forwarded-Proto header
+    # the number of gunicorn workers
+    'workers': 3,
+    # detect HTTPS mode from X-Forwarded-Proto header
+    'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -40,4 +41,6 @@ EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-ALLOWED_HOSTS=['*']
+GITHUB_APP_ID = '9f045cdbc059bfdf5869'
+GITHUB_API_SECRET = 'a540ff1fc93b5131f10ef6411972d0014c1110f3'
+GITHUB_EXTENDED_PERMISSIONS = ['vidahealth/webserver']
