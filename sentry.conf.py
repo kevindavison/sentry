a71646defe46a4fa31598c72655027603284d12c
Ozan Onay <ozan.onay@gmail.com>
2014-10-23 12:22:18 -0700

    Use mailgun and DATABASE_URL

--- sentry.conf.py
+++ sentry.conf.py
@@ -8,14 +8,7 @@ import dj_database_url
 SECRET_KEY = '*a&r#ql7pii7awl*xre(&j1@q-z4j2)@l)o5hu7=^emno!hx1%'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
+    'default': dj_database_url.config()
 }
 
 SENTRY_KEY = os.environ.get(
@@ -29,8 +22,6 @@ SERVER_EMAIL = os.environ.get('SENTRY_EMAIL_FROM', 'root@localhost')
 
 # The absolute URI to Sentry
 SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX')
-print "SENTRY URL PREFIX IS"
-print SENTRY_URL_PREFIX
 
 SENTRY_WEB_HOST = '0.0.0.0'
 SENTRY_WEB_PORT = 3000
@@ -43,13 +34,9 @@ SENTRY_WEB_OPTIONS = {
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
-print EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
+MAILGUN_ACCESS_KEY = os.environ['MAILGUN_ACCESS_KEY']
+MAILGUN_SERVER_NAME = os.environ['MAILGUN_SERVER_NAME']
 
 GITHUB_APP_ID = '9f045cdbc059bfdf5869'
 GITHUB_API_SECRET = 'a540ff1fc93b5131f10ef6411972d0014c1110f3'
