5c2fd05123665b950bc017541d4e7f0a123bdb5d
Ozan Onay <ozan.onay@gmail.com>
2014-10-23 14:22:45 -0700

    Fixes vidahealth/webserver#385. Force Sentry to use https

---
+++ middleware.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.http import HttpResponsePermanentRedirect
+
+
+class ForceSSLMiddleware(object):
+    """
+    Force redirect http:// urls to https://
+    """
+    def process_request(self, request):
+        if settings.DEBUG or request.is_secure():
+            return None
+
+        url = request.build_absolute_uri(request.get_full_path())
+        https_url = url.replace('http://', 'https://', 1)
+        return HttpResponsePermanentRedirect(https_url)
